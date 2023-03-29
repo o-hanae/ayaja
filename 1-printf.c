d3f8d8c534a73da0f2b26b8e2f55303d6c873b80
hiba-M <hiba.mouzain@gmail.com>
2023-03-28 18:18:40 -0700

    prints!?

---
+++ 1-printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_chr - Prints a character to stdout
+ * @args: A va_list containing the character to print
+ * Return: The number of characters printed
+ */
+
+int print_chr(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+
+/**
+ * print_str - Prints a string to stdout
+ * @args: A va_list containing the string to print
+ * Return: The number of characters printed
+ */
+int print_str(va_list args)
+{
+	int count = 0;
+	char *s = va_arg(args, char *);
+
+	if (!s)
+		s = "(null)";
+
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+		count++;
+	}
+	return (count);
+
+}
